45b52ed9755b411236c0a1a7e23841a9ec067a08
nithyav2017 <nithya.jganesh@gmail.com>
2020-04-28 16:36:42 -0500

    UOW Implementation

---
+++ UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using DataLayer.Repository;
+using MessageRelay.DataAccess;
+using MessageRelay.DataAccess.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+
+        // Save pending changes to the data store.
+        void Commit();
+
+        // Repositories
+        IRepoMessageQueue MessageQueuesRepository { get; }
+        IRepository<Template> Templates { get; }
+        IRepository<MessageType> MessageType { get; }
+        IRepository<MessageQueue> MessageQueue { get; }
+    }
+}
---
+++ UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using DataLayer.Repository;
+using MessageRelay.DataAccess;
+using MessageRelay.DataAccess.Repository;
+
+namespace DataLayer.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private MessagingEntities DbContext { get; set; }
+
+
+        public UnitOfWork()
+        {
+            CreateDbContext();
+          
+        }
+        private IRepoMessageQueue _messagequeue;
+
+        private IRepository<Template> _Templates;
+        private IRepository<MessageQueue> _MessageQueue;
+        private IRepository<MessageType> _MessageType;
+
+        public IRepoMessageQueue MessageQueuesRepository
+        {
+            get
+            {
+                if (_messagequeue == null)
+                {
+                    _messagequeue = new RepoMessageQueue (DbContext);
+                }
+                return _messagequeue;
+            }
+
+        }
+
+        public IRepository<Template> Templates
+        {
+            get
+            {
+                if (_Templates == null)
+                {
+                    _Templates = new Repository<Template>(DbContext);
+                }
+                return _Templates;
+            }
+
+        }
+
+        public IRepository<MessageQueue> MessageQueue
+        {
+            get
+            {
+                if (_MessageQueue == null)
+                {
+                    _MessageQueue = new Repository<MessageQueue>(DbContext);
+                }
+                return _MessageQueue;
+            }
+
+        }
+
+        public IRepository<MessageType> MessageType
+        {
+            get
+            {
+                if (_MessageType == null)
+                {
+                    _MessageType = new Repository<MessageType>(DbContext);
+                }
+                return _MessageType;
+            }
+
+        }
+
+
+
+        public void Commit()
+        {
+            DbContext.SaveChanges();
+        }
+        protected void CreateDbContext()
+        {
+            DbContext = new MessagingEntities();
+                         
+            DbContext.Configuration.ProxyCreationEnabled = false;
+
+            DbContext.Configuration.LazyLoadingEnabled = false;
+            
+            DbContext.Configuration.ValidateOnSaveEnabled = false;
+            
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (DbContext != null)
+                {
+                    DbContext.Dispose();
+                }
+            }
+        }
+
+    }
+}
